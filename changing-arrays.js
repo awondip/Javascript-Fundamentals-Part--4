dba033438d0184437c1c59d2d56a0508befea4b3
dylane <awondipdylane047@.com>
2022-03-30 15:16:03 +0100

    Changing Arrays

---
+++ changing-arrays.js
@@ -0,0 +1,4 @@
+const items = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+items[0] = 'tahini';
+console.log(items);
+// shopping will now return [ "tahini", "milk", "cheese", "hummus", "noodles" ]
